81a1eb001235ca51eaa6121ae51ba6cc45cbc06c
Sylvain Gadrat <sgadrat@wontfix.it>
2019-01-19 11:24:46 +0100

    Dummy Glutock ESP firmware

--- src/boards/unicorn.cpp
+++ src/boards/unicorn.cpp
@@ -1,37 +1,88 @@
 #include "mapinc.h"
 #include "../ines.h"
 
+#include <deque>
+
 class EspFirmware {
 public:
-	EspFirmware();
-	~EspFirmware();
-	void tx(uint8 v);
-	uint8 rx();
+	virtual ~EspFirmware() = default;
+
+	// Communication pins (dont care about UART details, directly transmit bytes)
+	virtual void rx(uint8 v);
+	virtual uint8 tx();
+
+	// General purpose I/O pins
+	void setGpio0(bool v);
+	bool getGpio0();
+	void setGpio2(bool v);
+	bool getGpio2();
+
+private:
+	bool gpio0 = false;
+	bool gpio2 = false;
 };
 
-EspFirmware::EspFirmware() {
-	//TODO setup network
-}
-
-EspFirmware::~EspFirmware() {
-	//TODO clean network structures
-}
-
-void EspFirmware::tx(uint8 v) {
-	//TODO interpret message
+void EspFirmware::rx(uint8 v) {
 	FCEU_printf("EspFirmware TX %02x\n", v);
 }
 
-uint8 EspFirmware::rx() {
-	//TODO figure what to do
+uint8 EspFirmware::tx() {
 	FCEU_printf("EspFirmware RX\n");
+	return 0;
 }
 
-static uint8 latche, latcheinit, bus_conflict;
-static uint16 addrreg0, addrreg1;
+void EspFirmware::setGpio0(bool v) {
+	this->gpio0 = v;
+}
+
+bool EspFirmware::getGpio0() {
+	return this->gpio0;
+}
+
+void EspFirmware::setGpio2(bool v) {
+	this->gpio2 = v;
+}
+
+bool EspFirmware::getGpio2() {
+	return this->gpio2;
+}
+
+class GlutockFirmware: public EspFirmware {
+public:
+	GlutockFirmware();
+	~GlutockFirmware();
+
+	void rx(uint8 v) override;
+	uint8 tx() override;
+
+private:
+	std::deque<uint8> rx_buffer;
+	std::deque<uint8> tx_buffer;
+};
+
+GlutockFirmware::GlutockFirmware() {
+	//TODO initialize network stuff
+	FCEU_printf("UNICORN GlutockFirmware ctor\n");
+}
+
+GlutockFirmware::~GlutockFirmware() {
+	//TODO clear network stuff
+	FCEU_printf("UNICORN GlutockFirmware dtor\n");
+}
+
+void GlutockFirmware::rx(uint8 v) {
+	//TODO store byte in buffer, try to parse message from buffer, process parsed message
+	FCEU_printf("UNICORN GlutockFirmware rx %02x\n", v);
+}
+
+uint8 GlutockFirmware::tx() {
+	//TODO refresh buffer from network and get byte from buffer
+	FCEU_printf("UNICORN GlutockFirmware tx\n");
+	return 0;
+}
+
 static uint8 *WRAM = NULL;
 static uint32 WRAMSIZE;
-static void (*WSync)(void);
 static EspFirmware *esp = NULL;
 
 static void LatchClose(void) {
@@ -39,16 +90,18 @@ static void LatchClose(void) {
 	if (WRAM)
 		FCEU_gfree(WRAM);
 	WRAM = NULL;
+
+	delete esp;
 }
 
 static DECLFW(UNICORNWrite) {
 	FCEU_printf("UNICORN write %04x %02x\n", A, V);
-	esp->tx(V);
+	esp->rx(V);
 }
 
 static DECLFR(UNICORNRead) {
 	FCEU_printf("UNICORN read %04x\n", A);
-	return esp->rx();
+	return esp->tx();
 }
 
 static void UNICORNPower(void) {
@@ -66,6 +119,8 @@ static void UNICORNPower(void) {
 
 	SetWriteHandler(0x5000, 0x5000, UNICORNWrite);
 	SetReadHandler(0x5000, 0x5000, UNICORNRead);
+
+	esp = new GlutockFirmware;
 }
 
 void UNICORN_Init(CartInfo *info) {
